3ffde2ad12e57486ba990392811df1a8828d4aa0
pdmurray <peynmurray@gmail.com>
2025-05-30 16:19:47 -0700

    Make conda use the "tui" console

--- conda_declarative/util.py
+++ conda_declarative/util.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from argparse import Namespace
 from collections.abc import Generator
 from contextlib import contextmanager
+
+from conda.base.context import reset_context
 
 
 @contextmanager
@@ -15,8 +18,6 @@ def set_conda_console() -> Generator[None, None, None]:
     Generator[None, None, None]
         An empty generator is yielded here to defer environment cleanup
     """
-    # reset_context(
-    #     argparse_args=Namespace(console="classic")
-    # )
+    reset_context(argparse_args=Namespace(console="tui"))
     yield
-    # reset_context()
+    reset_context()
