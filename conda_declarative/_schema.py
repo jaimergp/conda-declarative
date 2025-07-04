8066b52a77e5885da6f0ab00fc66fafd329f8d4a
jaimergp <jaimergp@users.noreply.github.com>
2025-07-05 00:26:51 +0200

    Start Pydantic model to generate JSON schemas for `conda.toml`

---
+++ conda_declarative/_schema.py
@@ -0,0 +1,235 @@
+"""
+Pydantic models to generate the JSON schema behind the `conda.toml` format.
+"""
+
+from __future__ import annotations
+
+import json
+import re
+from enum import StrEnum
+from inspect import cleandoc
+from pathlib import Path
+from typing import Annotated, Literal, TypeAlias, Union  # noqa
+
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+HERE = Path(__file__).parent
+SCHEMA_PATH = HERE / "data" / "conda-manifest.schema.json"
+NAME_REGEX = VERSION_REGEX = r"^[a-zA-Z0-9_]([a-zA-Z0-9._-]*[a-zA-Z0-9_])?$"
+ENV_NAME_REGEX = r"^[^/:# ]+$"
+NonEmptyStr = Annotated[str, Field(min_length=1)]
+_base_config_dict = ConfigDict(
+    extra="forbid",
+    use_attribute_docstrings=True,
+)
+
+
+class ValidPlatforms(StrEnum):
+    """
+    Valid platforms (all known subdirs, except `noarch`).
+    """
+
+    EMSCRIPTEN_WASM32 = "emscripten-wasm32"
+    WASI_WASM32 = "wasi-wasm32"
+    FREEBSD_64 = "freebsd-64"
+    LINUX_32 = "linux-32"
+    LINUX_64 = "linux-64"
+    LINUX_AARCH64 = "linux-aarch64"
+    LINUX_ARMV6L = "linux-armv6l"
+    LINUX_ARMV7L = "linux-armv7l"
+    LINUX_PPC64 = "linux-ppc64"
+    LINUX_PPC64LE = "linux-ppc64le"
+    LINUX_RISCV64 = "linux-riscv64"
+    LINUX_S390X = "linux-s390x"
+    OSX_64 = "osx-64"
+    OSX_ARM64 = "osx-arm64"
+    WIN_32 = "win-32"
+    WIN_64 = "win-64"
+    WIN_ARM64 = "win-arm64"
+    ZOS_Z = "zos-z"
+
+
+class Author(BaseModel):
+    """
+    Metadata to describe a single author or maintainer.
+    """
+
+    model_config: ConfigDict = _base_config_dict
+
+    name: NonEmptyStr
+    """
+    Name of the author or team.
+    """
+    email: EmailStr
+    """
+    E-mail address for the author or team.
+    """
+
+
+class Project(BaseModel):
+    """
+    Metadata about for this manifest file. Mostly static.
+    """
+
+    model_config: ConfigDict = _base_config_dict
+
+    name: NonEmptyStr = ...
+    """
+    Name for this project.
+    """
+    revision: NonEmptyStr = ...
+    """
+    A version-like string that allows maintainers to apply versioning schemes to this manifest file.
+    """
+    description: NonEmptyStr | None = None
+    """
+    An optional free-form text field for maintainers to document this manifest file.
+    Supports Markdown.
+    """
+    authors: list[Author] = []
+    """
+    People or teams involved in the creation and maintenance of this file.
+    """
+    license: NonEmptyStr
+    """
+    SPDX expression describing the license of this work.
+    """
+    license_files: list[NonEmptyStr] = Field([], alias="license-files")
+    """
+    List of paths to the license files governing this work.
+    """
+    urls: dict[NonEmptyStr, NonEmptyStr] = {}
+    """
+    Mapping of website names to their URL addresses.
+    """
+
+
+class SystemRequirements(BaseModel):
+    """
+    Properties of system features.
+
+    These will be exposed to the solver for the appropriate
+    selection of package variants, usually as lower bounds.
+    """
+
+    model_config: ConfigDict = _base_config_dict
+
+    libc: NonEmptyStr | None = None
+    "Version of the system libc (Linux only). Equivalent to setting `CONDA_OVERRIDE_GLIBC`."
+    cuda: NonEmptyStr | None = None
+    "Version of CUDA. Equivalent to setting `CONDA_OVERRIDE_CUDA`."
+    osx: NonEmptyStr | None = None
+    "Version of macOS. Equivalent to setting `CONDA_OVERRIDE_OSX`."
+    linux: NonEmptyStr | None = None
+    "Version of the Linux kernel. Equivalent to setting `CONDA_OVERRIDE_LINUX`."
+    win: NonEmptyStr | None = None
+    "Version of Windows in use. Equivalent to setting `CONDA_OVERRIDE_WIN`."
+    archspec: NonEmptyStr | None = None
+    "System architecture name. Equivalent to setting `CONDA_OVERRIDE_ARCHSPEC`."
+
+
+class CondaPackageConstraints(BaseModel):
+    """
+    Constraints for conda packages.
+    """
+
+    model_config: ConfigDict = _base_config_dict
+
+    version: NonEmptyStr = "*"
+    build: NonEmptyStr = "*"
+    channel: NonEmptyStr | None = None
+
+
+class PlatformSpecificFields(BaseModel):
+    """
+    Platform-specific overrides.
+    """
+
+    model_config: ConfigDict = _base_config_dict
+
+    config: dict[NonEmptyStr, object] = {}  # TODO: Specify all accepted settings
+    """
+    Configuration details for the install tool.
+    """
+    system_requirements: SystemRequirements | None = Field(
+        None,
+        alias="system-requirements",
+        help=SystemRequirements.__doc__,
+    )
+    dependencies: dict[NonEmptyStr, NonEmptyStr | CondaPackageConstraints] = {}
+    """
+    conda packages to install. It must be a mapping of package names to package versions.
+    Use `*` for the version if any version works. The value can also be an object that
+    specifies version, build and/or channel.
+    """
+    constraints: dict[NonEmptyStr, NonEmptyStr | CondaPackageConstraints] = {}
+    """
+    Additional constraints to impose on the solver for conda dependencies.
+
+    These are not requirements, but will condition which packages are available for selection.
+    """
+    pypi_dependencies: dict[NonEmptyStr, NonEmptyStr] = Field(
+        {}, alias="pypi-dependencies"
+    )
+    """
+    PyPI packages to install. It must be a mapping of package names to package versions.
+    Use `*` for the version if any version works.
+    """
+
+
+class CondaManifest(PlatformSpecificFields):
+    """
+    Schema for `conda.toml` manifest files.
+    """
+
+    model_config: ConfigDict = _base_config_dict
+
+    schema_: Annotated[str, Field(min_length=1, alias="$schema")] = (
+        "https://schemas.conda.org/conda/v0/conda-manifest.schema.json"
+    )
+    """
+    JSON Schema URL or path used to validate this input file.
+    """
+    version: Annotated[int, Field(ge=0)] = 0
+    """
+    Version of the conda.toml file format.
+    """
+    about: Project = Field(help=Project.__doc__)
+    platform: dict[ValidPlatforms, PlatformSpecificFields] = {}
+    """
+    Platform-specific configuration.
+    """
+
+
+def fix_descriptions(obj: dict) -> dict:
+    for key, value in obj.items():
+        if isinstance(value, dict):
+            obj[key] = fix_descriptions(value)
+        if key == "description" and isinstance(value, str):
+            codeblocks = re.findall(r"```.*```", value, flags=re.MULTILINE | re.DOTALL)
+            for i, codeblock in enumerate(codeblocks):
+                value = value.replace(codeblock, f"__CODEBLOCK_{i}__")
+            value = (
+                value.replace("\n\n", "__NEWLINE__")
+                .replace("\n-", "__NEWLINE__-")
+                .replace("\n", " ")
+                .replace("  ", " ")
+                .replace("__NEWLINE__", "\n")
+            )
+            for i, codeblock in enumerate(codeblocks):
+                value = value.replace(f"__CODEBLOCK_{i}__", codeblock)
+            obj[key] = value
+
+    return obj
+
+
+def dump_schema() -> None:
+    obj = CondaManifest.model_json_schema()
+    obj = fix_descriptions(obj)
+    obj["$schema"] = "https://json-schema.org/draft/2020-12/schema"
+    SCHEMA_PATH.write_text(json.dumps(obj, sort_keys=True, indent=2) + "\n")
+    print(json.dumps(obj, sort_keys=True, indent=2))
+
+
+if __name__ == "__main__":
+    dump_schema()
